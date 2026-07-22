bf4848671ed1ec001a8b3f3eb62e3133e42a3d8f
ManahilMuhammad <151027762+ManahilMuhammad@users.noreply.github.com>
2026-07-22 23:12:32 +0300

    refactored code for clarity

--- BE-01/server.js
+++ BE-01/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const openapiSpec = require('./openapi.json');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-
-const swaggerUi = require('swagger-ui-express');
-const openapiSpec = require('./openapi.json');
-
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiSpec));
 
 // list of tasks in memory
@@ -123,6 +121,7 @@ app.delete('/tasks/:id', (req, res) => {
     return res.status(204).send();
 });
 
+// start server
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Task API listening on port ${port}`);
 });
