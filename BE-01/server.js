887cef8094a0490facf2ef2ad9c0bfe655a6bb27
ManahilMuhammad <151027762+ManahilMuhammad@users.noreply.github.com>
2026-07-22 08:36:43 +0300

    Stage 2: read endpoints with 404

--- BE-01/server.js
+++ BE-01/server.js
@@ -2,6 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// list of tasks in memory
+let inMemory = [
+    {
+        "id": 0,
+        "title": "Write project report",
+        "done": false
+    },
+    {
+        "id": 1,
+        "title": "Review code",
+        "done": false
+    },
+    {
+        "id": 2,
+        "title": "Finish Stage 2",
+        "done": true
+    }
+]
+
 app.get('/', (req, res) => {
     apiDescription = {
         "name": "Task API",
@@ -11,8 +30,27 @@ app.get('/', (req, res) => {
     res.json(apiDescription)
 });
 
+// health
 app.get('/health', (req, res) => {
     res.status(200).json({ status: 'ok' });
+});
+
+// tasks -- return all tasks
+app.get('/tasks', (req, res) => {
+    res.json(inMemory)
+});
+
+// return task with specified ID
+app.get('/tasks/:id', (req, res) => {
+    const ID = req.params.id;
+    let task = inMemory.find(task => task.id == ID);
+    let errorMessage = `Task ${ID} not found`;
+    
+    if (task == undefined) {
+        return res.status(404).json({ "error": errorMessage });
+    }
+
+    res.json(task)
 });
 
 app.listen(port, () => {
