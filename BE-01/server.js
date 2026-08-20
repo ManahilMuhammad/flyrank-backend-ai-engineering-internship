718c23f9b7cd5eb4dfd79d8fbb06d0bd7a7e729c
ManahilMuhammad <151027762+ManahilMuhammad@users.noreply.github.com>
2026-08-20 13:54:17 +0300

    split assignments to different ports

--- BE-01/server.js
+++ BE-01/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const openapiSpec = require('./openapi.json');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiSpec));
